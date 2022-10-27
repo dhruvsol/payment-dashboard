e936954ac4d09532841b311eb73a2addd1e985c1
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2022-10-27 20:00:28 +0530

    feat:added

--- pages/index.tsx
+++ pages/index.tsx
@@ -207,6 +207,7 @@ const Home: NextPage = () => {
               <h1>private_api_key:- {apikeys?.public_api_key}</h1>
 
               <button
+                className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 onClick={() => {
                   router.push("/dashboard");
                 }}

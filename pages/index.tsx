95824a0f1732c53a26d6010bc88c9de8fad0e257
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2022-10-28 20:17:52 +0530

    feat:added new login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import { useWallet } from "@solana/wallet-adapter-react";
+import { useUser } from "use-supabase-hooks";
 import {
   useWalletModal,
   WalletMultiButton,
@@ -10,212 +11,123 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { toast, Toaster } from "react-hot-toast";
 import { supabase } from "../config/supabase";
-
+import * as bs58 from "bs58";
 const Home: NextPage = () => {
-  const [authState, setAuthState] = useState<"SIGNIN" | "SIGNUP">("SIGNIN");
   const [newUserState, setNewUserState] = useState<
     "SignUp" | "wallet-connet" | "api-key"
   >("SignUp");
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [businessname, setBusinessname] = useState<string>("");
+
   const router = useRouter();
-  const [apikeys, setApiKeys] = useState<any>(null);
-  const { connected, publicKey } = useWallet();
-  const getKey = async () => {
-    const { data } = await axios.post("/api/api-key", {
-      email,
-      wallet: publicKey?.toString(),
-      username: email,
-      avatar: "https://candypay.fun/logo.png",
-    });
-    setApiKeys(data);
-  };
-  useEffect(() => {
-    if (connected && apikeys === null) {
-      getKey();
-      setNewUserState("api-key");
-    }
-  }, [connected]);
-  async function signInWithEmail() {
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email: email,
-      password: password,
-    });
-    console.log(data);
-
-    const token = await supabase.auth.getSession();
-
-    const a = await axios
-      .post("/api/user", {
-        token: token.data.session?.access_token,
-        email: email,
-      })
-      .then(async (a) => {
-        if (a.data.metadata === null) {
-          setNewUserState("wallet-connet");
-        } else {
-          router.push("/dashboard");
+  const { connected, publicKey, signMessage } = useWallet();
+  const PostNewUser = async (sign: any) => {
+    const user = await supabase.auth.getUser();
+    const { data } = await supabase.auth.getSession();
+    await axios
+      .post(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/onboard`,
+        {
+          email: user.data.user?.email,
+          username: user.data.user?.user_metadata.name,
+          wallet_address: publicKey?.toString(),
+          avatar: user.data.user?.user_metadata.picture,
+          business_name: businessname,
+          signature: sign,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${data.session?.access_token}`,
+          },
         }
+      )
+      .then(() => {
+        router.push("/dashboard");
       });
-  }
-  async function signUpWithEmail() {
-    const { data } = await supabase.auth.signUp({
-      email: email,
-      password: password,
+  };
+
+  async function signInWithGoogle() {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
     });
-    if (data.user?.identities?.length) {
-      toast.success("Verfiy your email and Sign In");
-      // please confirm your email, or send again
-    } else {
-      toast.error("You already Have a account");
-      // already signed up, sign in instead?
-    }
   }
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((e) => {
+      if (e === "SIGNED_IN") {
+        setNewUserState("wallet-connet");
+      }
+    });
+  }, []);
+  const signthing = async () => {
+    const message = new TextEncoder().encode("CANDYPAY");
+    // Sign the bytes using the wallet
+    const signature = await signMessage!(message);
+    // const n = new TextEncoder().encoding(signature);
+    const aa = bs58.encode(signature).toString();
+    console.log(aa);
+
+    await PostNewUser(aa);
+
+    // console.log(signature);
+  };
   return (
     <>
       <div className="flex min-h-screen  flex-col  justify-center py-12 sm:px-6 lg:px-8">
         {newUserState === "SignUp" && (
           <>
             <div className="flex justify-center text-4xl font-bold">
-              <h1>{authState === "SIGNIN" ? "Sign In" : "Sign Up"}</h1>
+              <h1>Login</h1>
             </div>
             <div className="mt-8 sm:mx-auto border-2 rounded-lg sm:w-full sm:max-w-md">
-              <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                <form
-                  className="space-y-6"
-                  onSubmit={(e) => {
-                    e.preventDefault();
-                    if (authState === "SIGNIN") {
-                      signInWithEmail();
-                    } else {
-                      signUpWithEmail();
-                    }
+              <div className="bg-white py-8 px-4 flex flex-col gap-10 shadow sm:rounded-lg sm:px-10">
+                <button
+                  onClick={() => {
+                    signInWithGoogle().then(() => {
+                      console.log("helo");
+                    });
                   }}
+                  className="bg-orange-400 w-full h-12 rounded-lg"
                 >
-                  <div>
-                    <label
-                      htmlFor="email"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      Email address
-                    </label>
-                    <div className="mt-1">
-                      <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        autoComplete="email"
-                        required
-                        onChange={(e) => {
-                          setEmail(e.target.value);
-                        }}
-                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                      />
-                    </div>
-                  </div>
-
-                  <div>
-                    <label
-                      htmlFor="password"
-                      className="block text-sm font-medium text-gray-700"
-                    >
-                      Password
-                    </label>
-                    <div className="mt-1">
-                      <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        onChange={(e) => {
-                          setPassword(e.target.value);
-                        }}
-                        autoComplete="current-password"
-                        required
-                        className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                      />
-                    </div>
-                  </div>
-
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center">
-                      <input
-                        id="remember-me"
-                        name="remember-me"
-                        type="checkbox"
-                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                      />
-                      <label
-                        htmlFor="remember-me"
-                        className="ml-2 block text-sm text-gray-900"
-                      >
-                        Remember me
-                      </label>
-                    </div>
-
-                    <div className="text-sm">
-                      <a
-                        href="#"
-                        className="font-medium text-indigo-600 hover:text-indigo-500"
-                      >
-                        Forgot your password?
-                      </a>
-                    </div>
-                  </div>
-
-                  <div>
-                    <button
-                      type="submit"
-                      className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                    >
-                      Sign in
-                    </button>
-                  </div>
-                </form>
-                <div className="flex justify-end mt-3">
-                  <button
-                    onClick={() => {
-                      if (authState === "SIGNIN") {
-                        setAuthState("SIGNUP");
-                      } else {
-                        setAuthState("SIGNIN");
-                      }
-                    }}
-                    className="text-base font-semibold"
-                  >
-                    {authState === "SIGNUP"
-                      ? "Already Have a account ?"
-                      : "Create One ?"}
-                  </button>
-                </div>
+                  Login with google
+                </button>
+                <button className="bg-blue-500 w-full h-12 rounded-lg">
+                  Login with twitter
+                </button>
               </div>
             </div>
           </>
         )}
         {newUserState === "wallet-connet" && (
           <div className="flex justify-center items-center flex-col">
-            <h1 className="text-2xl mb-10 font-extrabold">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Business Name
+              </label>
+              <div className="mt-1 w-80">
+                <input
+                  type="text"
+                  onChange={(e) => {
+                    setBusinessname(e.target.value);
+                  }}
+                  className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                  placeholder="abcd...."
+                />
+              </div>
+            </div>
+            <h1 className="text-2xl my-5font-extrabold">
               Please Connect wallet Which you want to use
             </h1>
             <WalletMultiButton />
-          </div>
-        )}
-        {connected && newUserState === "api-key" && (
-          <>
-            <div className="flex gap-4 mt-20 justify-center items-center flex-col">
-              <h1>Your Api Keys are</h1>
-              <h1> private_api_key:- {apikeys?.private_api_key}</h1>
-              <h1>private_api_key:- {apikeys?.public_api_key}</h1>
 
-              <button
-                className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                onClick={() => {
-                  router.push("/dashboard");
-                }}
-              >
-                Continue
-              </button>
-            </div>
-          </>
+            <button
+              onClick={() => signthing()}
+              className="w-96 h-14 mt-12 text-white rounded-lg bg-violet-600"
+            >
+              Submit Details
+            </button>
+          </div>
         )}
       </div>
       <Toaster />
--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,12 +1,70 @@
+import axios from "axios";
 import { NextPage } from "next";
-import React from "react";
+import React, { useState } from "react";
+import { supabase } from "../../config/supabase";
 import MainLayout from "../../layouts";
 
 const Settings: NextPage = () => {
+  const [keys, setKeys] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const genrate = async () => {
+    setLoading(true);
+    const { data: session } = await supabase.auth.getSession();
+    const { data: userInfo } = await supabase.auth.getUser();
+    const user = await axios.post("/api/user", {
+      email: userInfo.user?.email,
+      token: session.session?.access_token,
+    });
+    const getapikey = await axios.post("/api/api-key", {
+      email: user.data.metadata.email,
+      wallet: user.data.metadata.wallet_address,
+      username: user.data.metadata.username,
+      avatar: user.data.metadata.avatar,
+    });
+    setKeys(getapikey.data);
+    setLoading(false);
+    console.log(getapikey.data);
+  };
   return (
     <>
       <MainLayout>
-        <div>Hello</div>
+        <div className="flex justify-end mt-10 mr-32">
+          <button
+            onClick={genrate}
+            className="bg-violet-500 text-white rounded-lg h-12 w-[13rem]"
+          >
+            {!loading ? "Genrate" : "loading..."}
+          </button>
+        </div>
+        {keys && (
+          <div className="mt-10 ml-14">
+            <div className="flex justify-start gap-10 items-center mb-10">
+              <h1 className="text-2xl font-medium  ">
+                The Keys will be shown once do to security reasons
+              </h1>
+              <button
+                onClick={() => {
+                  window.navigator.clipboard.writeText(
+                    JSON.stringify({
+                      private_key: keys?.private_api_key,
+                      public_key: keys?.public_api_key,
+                    })
+                  );
+                }}
+                className="h-10 w-20 bg-violet-500 font-bold text-white rounded"
+              >
+                copy
+              </button>
+            </div>
+
+            <h1 className="font-lg text-lg mb-2">
+              Private Keys: {keys?.private_api_key}
+            </h1>
+            <h1 className="font-lg text-lg">
+              Public Keys: {keys?.public_api_key}
+            </h1>
+          </div>
+        )}
       </MainLayout>
     </>
   );
